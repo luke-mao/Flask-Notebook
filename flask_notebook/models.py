3e962742c2e5e0f5c7a75a43458081e35576ca1b
luke-mao <55699548+luke-mao@users.noreply.github.com>
2020-09-26 21:39:25 +1000

    finish the edit and delete operations. now support multiple notes edit and delete

--- flask_notebook/models.py
+++ flask_notebook/models.py
@@ -10,8 +10,8 @@ class DBNotes(db.Model):
     # since we can modify the note
     # so create two columns: creation time and last modified time
     # during creation, both time are the same
-    current_time = datetime.now()
-    time_create = db.Column(db.DateTime, default=current_time)
-    time_last_modify = db.Column(db.DateTime, default=current_time, index=True)
+
+    time_create = db.Column(db.DateTime)
+    time_last_modify = db.Column(db.DateTime, index=True)
 
 
--- flask_notebook/views.py
+++ flask_notebook/views.py
@@ -6,6 +6,10 @@ from flask_notebook.models import DBNotes
 from flask_notebook.forms import NewNoteForm, EditNoteForm
 
 
+# global variable for the edit note function
+note_id_list = []
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # query the database for all notes
@@ -15,57 +19,99 @@ def index():
     form = NewNoteForm()
 
     if form.validate_on_submit():
+        # get current time
+        current = datetime.now()
+
         # obtain the body data
         body = form.body.data
 
         # build the record
-        new_note_to_db = DBNotes(body=body)
+        new_note_to_db = DBNotes(body=body, time_create=current, time_last_modify=current)
         db.session.add(new_note_to_db)
         db.session.commit()
 
         # render the page again
         # include the date and time
-        current = datetime.now()
-        flash(
-            'Your new note have been successfully recorded at {} {}:{}'.format(
-                current.date(), current.hour, current.minute
-            )
-        )
+
+        flash('Successfully create a new note at {}'.format(current.strftime('%Y-%m-%d %H:%M')))
         return redirect(url_for('index'))
 
     # if the form is not submitted, simply display the form and all notes
     return render_template('index.html', form=form, all_notes=all_notes)
 
 
-@app.route('/edit_notes', methods=['GET', 'POST'])
+# only allow post method
+@app.route('/edit_notes', methods=['POST'])
 def edit_notes():
-    # the selected note.id is in the request.form.getlist('select')
-    # print(request.form.getlist('select'))
+    # determine which button is pressed
+    # page can be from the 'edit' and 'delete' button from the index page
+    # or from the 'submit changes' from the edit_notes page
 
-    # print(request.method)
-    #
-    # if request.form.getlist('select'):
-    #     # chosen something to edit
-    #     print(request.form.getlist('select'))
-    # else:
-    #     print('no')
-    #
-    # return 'hello'
-
-    if request.form.getlist('select'):
+    if request.form.get('Edit'):
+        # the user selects several notes from the index page to update
+        # the user has submitted some updates, but maybe not all
+        # print(request.form.getlist('select')):
         # query the database and get these notes into a list
+        global note_id_list
+        note_id_list = [int(str_id) for str_id in request.form.getlist('select')]
         edit_note_list = []
         form_list = []
 
-        for note_id in request.form.getlist('select'):
-            this_note = DBNotes.query.get(int(note_id))
+        for note_id in note_id_list:
+            this_note = DBNotes.query.get(note_id)
             this_form = EditNoteForm()
+            this_form.body.label = note_id
             this_form.body.data = this_note.body
 
             edit_note_list.append(this_note)
             form_list.append(this_form)
 
         return render_template('edit_notes.html', edit_note_list=edit_note_list, form_list=form_list)
+
+    elif request.form.get('Delete'):
+        note_id_list_to_delete = [int(str_id) for str_id in request.form.getlist('select')]
+        for note_id in note_id_list_to_delete:
+            this_note =DBNotes.query.get(note_id)
+            db.session.delete(this_note)
+            db.session.commit()
+
+        flash('Successfully delete {} notes'.format(len(note_id_list_to_delete)))
+        return redirect(url_for('index'))
+
+    elif request.form.get('Submit Changes'):
+        # the user changes the form 'body' section
+        # after press submit button, the final data is in request.form
+        # in a form of immutable multi dict
+        # use getlist to make to a list
+        # the list should have same length as the note_id_list
+        new_body_list = request.form.getlist('body')
+
+        if len(new_body_list) != len(note_id_list):
+            return 'Error'
+
+        # get the modified time
+        current = datetime.now()
+
+        # count how many are changed
+        count = 0
+
+        for idx in range(len(note_id_list)):
+            # extract the original note
+            original_note = DBNotes.query.get(note_id_list[idx])
+            # if the body data is updated
+            if new_body_list[idx] != original_note.body:
+                original_note.body = new_body_list[idx]
+                original_note.time_last_modify = current
+                db.session.commit()
+                count += 1
+
+        # delete all content in the note_id_list
+        note_id_list = []
+
+        if count != 0:
+            flash('Successfully update {} notes at {}'.format(count, current.strftime('%Y-%m-%d %H:%M')))
+
+        return redirect(url_for('index'))
 
     else:
         return 'hello'
